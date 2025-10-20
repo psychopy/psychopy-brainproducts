6b3af28a6b71e4628181455a6e81efbbfa6de47f
Todd OST <todd.e.parsons@gmail.com>
2025-10-20 15:14:21 +0100

    SYS: Update docs to use packaged theme and be in correct folder

--- docs_src/conf.py
+++ docs_src/conf.py
@@ -47,8 +47,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme_path = ["themes"]
-# html_theme = 'psychopy_bootstrap'
 html_theme = 'psychopy_plugin'
 
 # HTML tab title
